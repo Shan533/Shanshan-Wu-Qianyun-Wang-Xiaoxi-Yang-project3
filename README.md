bbd3b97440f40c0dbbd1d86d04da9e96913c1949
Shanshan Wu <790219068@qq.com>
2024-04-22 14:08:20 -0700

    update readme

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Shanshan-Wu-Qianyun-Wang-Xiaoxi-Yang-project3
+
 CS5610 Project3 Password Manager
+
+## How to start
+
+### Client side
+
+npm run dev
+
+### Server side
+
+npx nodemon server/server.js
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,9 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useSelector } from "react-redux";
 import Spinner from "./components/Spinner";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
+import PasswordManager from "./pages/PasswordManager";
+import Register from "./pages/Register";
 
 function App() {
   const { loading } = useSelector((state) => state.loaders);
@@ -12,6 +15,9 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/main" element={<PasswordManager />} />
+          <Route path="/register" element={<Register />} />
         </Routes>
       </BrowserRouter>
     </>
