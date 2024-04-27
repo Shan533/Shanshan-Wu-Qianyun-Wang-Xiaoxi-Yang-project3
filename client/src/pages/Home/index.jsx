4eb9e99a32bd86be8491226d192ebcdeba81f4af
MochitheCoderCat <codingwithzai@gmail.com>
2024-04-26 20:16:24 -0700

    updated index page

--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -27,19 +27,19 @@ function Home() {
                 <Title level={3} className="green">
                   Feature 1
                 </Title>
-                <Paragraph>Secure password storage</Paragraph>
+                <Paragraph>Our website provides a secure vault for storing all your passwords with top-level encryption. Once saved, your passwords are protected with industry-standard security measures to prevent unauthorized access. You can access your stored passwords anytime, anywhere, with just a few clicks. This feature eliminates the need to remember multiple login details, simplifying your online experience.</Paragraph>
               </div>
               <div>
                 <Title level={3} className="green">
                   Feature 2
                 </Title>
-                <Paragraph>Generate strong passwords</Paragraph>
+                <Paragraph>Enhance your security with our password generator, which creates strong, hard-to-crack passwords for your accounts. Each password is a unique combination of letters, numbers, and symbols to maximize security. This tool helps you avoid common password pitfalls, such as using easily guessed passwords or repeating the same passwords across different sites. With our generator, you can ensure that each password is robust and secure.</Paragraph>
               </div>
               <div>
                 <Title level={3} className="green">
                   Feature 3
                 </Title>
-                <Paragraph>Share passwords securely</Paragraph>
+                <Paragraph>Our platform allows you to share passwords securely with family members, friends, or colleagues without compromising your security. Through encrypted channels, you can grant access to necessary passwords while maintaining control over who sees what information. This feature is ideal for collaborative environments, ensuring that sensitive information remains protected even when shared. Manage shared access easily through our user-friendly dashboard.</Paragraph>
               </div>
             </div>
           </div>
